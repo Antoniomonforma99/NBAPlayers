fdbe8c2256751fc726131a04b9de38a23554c166
Antonio Montero <antoniomonforma99@gmail.com>
2022-10-21 10:02:16 +0200

    ,

--- NBAPlayers/src/app/app.module.ts
+++ NBAPlayers/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { MaterialImportsModule } from './material-imports/material-imports.modul
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
