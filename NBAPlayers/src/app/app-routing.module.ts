c5b2bcbf1f0893cfde0437811fa0ab18d83903b6
Antonio Montero <antoniomonforma99@gmail.com>
2022-10-24 13:29:04 +0200

    cosas

--- NBAPlayers/src/app/app-routing.module.ts
+++ NBAPlayers/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlayersListComponent } from './components/players-list/players-list.component';
 import { TeamsListComponent } from './components/teams-list/teams-list.component';
 
 const routes: Routes = [
   {path: 'teams', component: TeamsListComponent},
   /*{path: 'inicio', component: HomeComponent},*/
   {path: '', redirectTo: '/inicio', pathMatch: 'full'},
+  {path: 'players', component: PlayersListComponent},
 
   
   /*{path: 'players', component: PlayersComponent},
@@ -13,6 +15,7 @@ const routes: Routes = [
   /*
   {path: '**', component: PageNotFoundComponent},
   */
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
